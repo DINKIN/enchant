bfc1477ec7353e9bceaad554a16d359035c5c630
Reuben Thomas <rrt@sc3d.org>
2017-02-21 15:25:35 +0000

    Revert "Improve HACKING"

--- src/myspell/myspell_checker.cpp
+++ src/myspell/myspell_checker.cpp
@@ -49,7 +49,7 @@
 #define MAXWORDLEN 176
 #endif
 
-ENCHANT_PLUGIN_DECLARE("Hunspell")
+ENCHANT_PLUGIN_DECLARE("Myspell")
 
 #define G_ICONV_INVALID (GIConv)-1
 
@@ -57,11 +57,11 @@ ENCHANT_PLUGIN_DECLARE("Hunspell")
 
 /***************************************************************************/
 
-class HunspellChecker
+class MySpellChecker
 {
 public:
-	HunspellChecker(EnchantBroker * broker);
-	~HunspellChecker();
+	MySpellChecker(EnchantBroker * broker);
+	~MySpellChecker();
 
 	bool checkWord (const char *word, size_t len);
 	char **suggestWord (const char* const word, size_t len, size_t *out_n_suggs);
@@ -71,12 +71,12 @@ public:
 private:
 	GIConv  m_translate_in; /* Selected translation from/to Unicode */
 	GIConv  m_translate_out;
-	Hunspell *hunspell;
+	Hunspell *myspell;
 	EnchantBroker *m_broker;
 };
 
 static void
-hunspell_checker_free_helper (gpointer p, gpointer user _GL_UNUSED_PARAMETER)
+myspell_checker_free_helper (gpointer p, gpointer user _GL_UNUSED_PARAMETER)
 {
 	g_free (p);
 }
@@ -89,14 +89,14 @@ g_iconv_is_valid(GIConv i)
 	return (i != G_ICONV_INVALID);
 }
 
-HunspellChecker::HunspellChecker(EnchantBroker * broker)
-: m_translate_in(G_ICONV_INVALID), m_translate_out(G_ICONV_INVALID), hunspell(0), m_broker(broker)
+MySpellChecker::MySpellChecker(EnchantBroker * broker)
+: m_translate_in(G_ICONV_INVALID), m_translate_out(G_ICONV_INVALID), myspell(0), m_broker(broker)
 {
 }
 
-HunspellChecker::~HunspellChecker()
+MySpellChecker::~MySpellChecker()
 {
-	delete hunspell;
+	delete myspell;
 	if (g_iconv_is_valid (m_translate_in ))
 		g_iconv_close(m_translate_in);
 	if (g_iconv_is_valid(m_translate_out))
@@ -104,7 +104,7 @@ HunspellChecker::~HunspellChecker()
 }
 
 bool
-HunspellChecker::checkWord(const char *utf8Word, size_t len)
+MySpellChecker::checkWord(const char *utf8Word, size_t len)
 {
 	if (len > MAXWORDLEN || !g_iconv_is_valid(m_translate_in))
 		return false;
@@ -121,14 +121,14 @@ HunspellChecker::checkWord(const char *utf8Word, size_t len)
 	if (static_cast<size_t>(-1) == result)
 		return false;
 	*out = '\0';
-	if (hunspell->spell(word8))
+	if (myspell->spell(word8))
 		return true;
 	else
 		return false;
 }
 
 char**
-HunspellChecker::suggestWord(const char* const utf8Word, size_t len, size_t *nsug)
+MySpellChecker::suggestWord(const char* const utf8Word, size_t len, size_t *nsug)
 {
 	if (len > MAXWORDLEN 
 		|| !g_iconv_is_valid(m_translate_in)
@@ -149,7 +149,7 @@ HunspellChecker::suggestWord(const char* const utf8Word, size_t len, size_t *nsu
 
 	*out = '\0';
 	char **sugMS;
-	*nsug = hunspell->suggest(&sugMS, word8);
+	*nsug = myspell->suggest(&sugMS, word8);
 	if (*nsug > 0) {
 		char **sug = g_new0 (char *, *nsug + 1);
 		for (size_t i=0; i<*nsug; i++) {
@@ -178,7 +178,7 @@ HunspellChecker::suggestWord(const char* const utf8Word, size_t len, size_t *nsu
 }
 
 static GSList *
-hunspell_checker_get_dictionary_dirs (EnchantBroker * broker)
+myspell_checker_get_dictionary_dirs (EnchantBroker * broker)
 {
 	GSList *dirs = NULL;
 
@@ -190,10 +190,10 @@ hunspell_checker_get_dictionary_dirs (EnchantBroker * broker)
 		for (iter = config_dirs; iter; iter = iter->next)
 			{
 				dirs = g_slist_append (dirs, g_build_filename (static_cast<const gchar *>(iter->data), 
-									       "hunspell", nullptr));
+									       "myspell", nullptr));
 			}
 
-		g_slist_foreach (config_dirs, hunspell_checker_free_helper, nullptr);
+		g_slist_foreach (config_dirs, myspell_checker_free_helper, nullptr);
 		g_slist_free (config_dirs);
 	}
 
@@ -203,7 +203,7 @@ hunspell_checker_get_dictionary_dirs (EnchantBroker * broker)
 
 		for (iter = system_data_dirs; *iter; iter++)
 			{
-				dirs = g_slist_append (dirs, g_build_filename (*iter, "hunspell", "dicts", nullptr));
+				dirs = g_slist_append (dirs, g_build_filename (*iter, "myspell", "dicts", nullptr));
 			}
 	}
 
@@ -211,26 +211,26 @@ hunspell_checker_get_dictionary_dirs (EnchantBroker * broker)
 	char * enchant_prefix = enchant_get_prefix_dir();
 	if(enchant_prefix)
 		{
-			char * hunspell_prefix = g_build_filename(enchant_prefix, "share", "enchant", "hunspell", nullptr);
+			char * myspell_prefix = g_build_filename(enchant_prefix, "share", "enchant", "myspell", nullptr);
 			g_free(enchant_prefix);
-			dirs = g_slist_append (dirs, hunspell_prefix);
+			dirs = g_slist_append (dirs, myspell_prefix);
 		}
 
-#ifdef ENCHANT_HUNSPELL_DICT_DIR
-	dirs = g_slist_append (dirs, g_strdup (ENCHANT_HUNSPELL_DICT_DIR));
+#ifdef ENCHANT_MYSPELL_DICT_DIR
+	dirs = g_slist_append (dirs, g_strdup (ENCHANT_MYSPELL_DICT_DIR));
 #endif
 
 	{
 		GSList *config_dirs, *iter;
 
-		config_dirs = enchant_get_dirs_from_param (broker, "enchant.hunspell.dictionary.path");
+		config_dirs = enchant_get_dirs_from_param (broker, "enchant.myspell.dictionary.path");
 		
 		for (iter = config_dirs; iter; iter = iter->next)
 			{
 				dirs = g_slist_append (dirs, g_strdup (static_cast<const gchar *>(iter->data)));
 			}
 
-		g_slist_foreach (config_dirs, hunspell_checker_free_helper, nullptr);
+		g_slist_foreach (config_dirs, myspell_checker_free_helper, nullptr);
 		g_slist_free (config_dirs);
 	}
 
@@ -248,18 +248,18 @@ hunspell_checker_get_dictionary_dirs (EnchantBroker * broker)
 static void
 s_buildDictionaryDirs (std::vector<std::string> & dirs, EnchantBroker * broker)
 {
-	GSList *hunspell_dirs, *iter;
+	GSList *myspell_dirs, *iter;
 
 	dirs.clear ();
 
-	hunspell_dirs = hunspell_checker_get_dictionary_dirs (broker);
-	for (iter = hunspell_dirs; iter; iter = iter->next)
+	myspell_dirs = myspell_checker_get_dictionary_dirs (broker);
+	for (iter = myspell_dirs; iter; iter = iter->next)
 		{
 			dirs.push_back (static_cast<const char *>(iter->data));
 		}
 
-	g_slist_foreach (hunspell_dirs, hunspell_checker_free_helper, nullptr);
-	g_slist_free (hunspell_dirs);
+	g_slist_foreach (myspell_dirs, myspell_checker_free_helper, nullptr);
+	g_slist_free (myspell_dirs);
 }
 
 static void
@@ -311,7 +311,7 @@ static bool is_plausible_dict_for_tag(const char *dir_entry, const char *tag)
 }
 
 static char *
-hunspell_request_dictionary (EnchantBroker * broker, const char * tag) 
+myspell_request_dictionary (EnchantBroker * broker, const char * tag) 
 {
 	std::vector<std::string> names;
 
@@ -351,11 +351,11 @@ hunspell_request_dictionary (EnchantBroker * broker, const char * tag)
 }
 
 bool
-HunspellChecker::requestDictionary(const char *szLang)
+MySpellChecker::requestDictionary(const char *szLang)
 {
 	char *dic = NULL, *aff = NULL;
 
-	dic = hunspell_request_dictionary (m_broker, szLang);
+	dic = myspell_request_dictionary (m_broker, szLang);
 	if (!dic)
 		return false;
 
@@ -364,14 +364,14 @@ HunspellChecker::requestDictionary(const char *szLang)
 	strcpy(aff+len_dic-3, "aff");
 	if (g_file_test(aff, G_FILE_TEST_EXISTS))
 	{
-		hunspell = new Hunspell(aff, dic);
+		myspell = new Hunspell(aff, dic);
 	}
 	g_free(dic);
 	g_free(aff);
-	if(hunspell == NULL){
+	if(myspell == NULL){
 		return false;
 	}
-	char *enc = hunspell->get_dic_encoding();
+	char *enc = myspell->get_dic_encoding();
 
 	m_translate_in = g_iconv_open(enc, "UTF-8");
 	m_translate_out = g_iconv_open("UTF-8", enc);
@@ -384,21 +384,21 @@ HunspellChecker::requestDictionary(const char *szLang)
  */
 
 static char **
-hunspell_dict_suggest (EnchantDict * me, const char *const word,
+myspell_dict_suggest (EnchantDict * me, const char *const word,
 		     size_t len, size_t * out_n_suggs)
 {
-	HunspellChecker * checker;
+	MySpellChecker * checker;
 	
-	checker = static_cast<HunspellChecker *>(me->user_data);
+	checker = static_cast<MySpellChecker *>(me->user_data);
 	return checker->suggestWord (word, len, out_n_suggs);
 }
 
 static int
-hunspell_dict_check (EnchantDict * me, const char *const word, size_t len)
+myspell_dict_check (EnchantDict * me, const char *const word, size_t len)
 {
-	HunspellChecker * checker;
+	MySpellChecker * checker;
 	
-	checker = static_cast<HunspellChecker *>(me->user_data);
+	checker = static_cast<MySpellChecker *>(me->user_data);
 	
 	if (checker->checkWord(word, len))
 		return 0;
@@ -407,7 +407,7 @@ hunspell_dict_check (EnchantDict * me, const char *const word, size_t len)
 }
 
 static void
-hunspell_provider_enum_dicts (const char * const directory,
+myspell_provider_enum_dicts (const char * const directory,
 			     std::vector<std::string> & out_dicts)
 {
 	GDir * dir = g_dir_open (directory, 0, nullptr);
@@ -449,7 +449,7 @@ ENCHANT_MODULE_EXPORT (EnchantProvider *)
 	     init_enchant_provider (void);
 
 static char ** 
-hunspell_provider_list_dicts (EnchantProvider * me, 
+myspell_provider_list_dicts (EnchantProvider * me, 
 			    size_t * out_n_dicts)
 {
 	std::vector<std::string> dict_dirs, dicts;
@@ -459,7 +459,7 @@ hunspell_provider_list_dicts (EnchantProvider * me,
 
 	for (size_t i = 0; i < dict_dirs.size(); i++)
 		{
-			hunspell_provider_enum_dicts (dict_dirs[i].c_str(), dicts);
+			myspell_provider_enum_dicts (dict_dirs[i].c_str(), dicts);
 		}
 
 	if (dicts.size () > 0) {
@@ -474,18 +474,18 @@ hunspell_provider_list_dicts (EnchantProvider * me,
 }
 
 static void
-hunspell_provider_free_string_list (EnchantProvider * me _GL_UNUSED_PARAMETER, char **str_list)
+myspell_provider_free_string_list (EnchantProvider * me _GL_UNUSED_PARAMETER, char **str_list)
 {
 	g_strfreev (str_list);
 }
 
 static EnchantDict *
-hunspell_provider_request_dict(EnchantProvider * me, const char *const tag)
+myspell_provider_request_dict(EnchantProvider * me, const char *const tag)
 {
 	EnchantDict *dict;
-	HunspellChecker * checker;
+	MySpellChecker * checker;
 	
-	checker = new HunspellChecker(me->owner);
+	checker = new MySpellChecker(me->owner);
 	
 	if (!checker)
 		return NULL;
@@ -497,26 +497,26 @@ hunspell_provider_request_dict(EnchantProvider * me, const char *const tag)
 	
 	dict = g_new0(EnchantDict, 1);
 	dict->user_data = (void *) checker;
-	dict->check = hunspell_dict_check;
-	dict->suggest = hunspell_dict_suggest;
+	dict->check = myspell_dict_check;
+	dict->suggest = myspell_dict_suggest;
 	// don't implement personal, session
 	
 	return dict;
 }
 
 static void
-hunspell_provider_dispose_dict (EnchantProvider * me _GL_UNUSED_PARAMETER, EnchantDict * dict)
+myspell_provider_dispose_dict (EnchantProvider * me _GL_UNUSED_PARAMETER, EnchantDict * dict)
 {
-	HunspellChecker *checker;
+	MySpellChecker *checker;
 	
-	checker = (HunspellChecker *) dict->user_data;
+	checker = (MySpellChecker *) dict->user_data;
 	delete checker;
 	
 	g_free (dict);
 }
 
 static int
-hunspell_provider_dictionary_exists (struct str_enchant_provider * me,
+myspell_provider_dictionary_exists (struct str_enchant_provider * me,
 				    const char *const tag)
 {
 	std::vector <std::string> names;
@@ -536,21 +536,21 @@ hunspell_provider_dictionary_exists (struct str_enchant_provider * me,
 }
 
 static void
-hunspell_provider_dispose (EnchantProvider * me)
+myspell_provider_dispose (EnchantProvider * me)
 {
 	g_free (me);
 }
 
 static const char *
-hunspell_provider_identify (EnchantProvider * me _GL_UNUSED_PARAMETER)
+myspell_provider_identify (EnchantProvider * me _GL_UNUSED_PARAMETER)
 {
-	return "hunspell";
+	return "myspell";
 }
 
 static const char *
-hunspell_provider_describe (EnchantProvider * me _GL_UNUSED_PARAMETER)
+myspell_provider_describe (EnchantProvider * me _GL_UNUSED_PARAMETER)
 {
-	return "Hunspell Provider";
+	return "Myspell Provider";
 }
 
 EnchantProvider *
@@ -559,14 +559,14 @@ init_enchant_provider (void)
 	EnchantProvider *provider;
 	
 	provider = g_new0(EnchantProvider, 1);
-	provider->dispose = hunspell_provider_dispose;
-	provider->request_dict = hunspell_provider_request_dict;
-	provider->dispose_dict = hunspell_provider_dispose_dict;
-	provider->dictionary_exists = hunspell_provider_dictionary_exists;
-	provider->identify = hunspell_provider_identify;
-	provider->describe = hunspell_provider_describe;
-	provider->free_string_list = hunspell_provider_free_string_list;
-	provider->list_dicts = hunspell_provider_list_dicts;
+	provider->dispose = myspell_provider_dispose;
+	provider->request_dict = myspell_provider_request_dict;
+	provider->dispose_dict = myspell_provider_dispose_dict;
+	provider->dictionary_exists = myspell_provider_dictionary_exists;
+	provider->identify = myspell_provider_identify;
+	provider->describe = myspell_provider_describe;
+	provider->free_string_list = myspell_provider_free_string_list;
+	provider->list_dicts = myspell_provider_list_dicts;
 
 	return provider;
 }
